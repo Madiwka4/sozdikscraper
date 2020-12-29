75d1a06899992b085f802a485c1e8e0a87f269ab
root <root@DESKTOP-S632B9T.localdomain>
2020-12-29 09:18:39 +0600

    cleaned junk

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,16 @@
-pip3 install tkinter
-pip3 install bs4
+echo "Your distro? ( 1)Arch, 2)Debian): "
+read distro
+if [ $distro -eq 1 ]
+then 
+    sudo pacman -S python3 python-pip html2text
+    pip install bs4
+    chmod +x ./sozdik.sh
+elif [ $distro -eq 2 ]
+then 
+    sudo apt install python3 python3-pip html2text
+    pip3 install bs4
+    chmod +x ./sozdik.sh 
+else 
+    echo "You typed in: $distro"
+    echo "Wrong input, please type one of the two: (1/2)";
+fi
