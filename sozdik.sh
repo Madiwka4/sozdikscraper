05d97b54ede48c6f08be7419e1d0b179eda9242a
root <root@DESKTOP-S632B9T.localdomain>
2020-12-29 09:58:16 +0600

    Added utf8 characters

--- sozdik.sh
+++ sozdik.sh
@@ -3,5 +3,5 @@ agent="Mozilla/5.0 (Windows NT 10.0; x86_64; rv:67.0) Gecko/20100101 Firefox/67.
 while true
 do 
 	read -p "Translate what word: " word
-	python3 scrape.py -i "$(echo "https://sozdik.kz/ru/dictionary/translate/ru/kk/"$word"/")"  | html2text
+	python3 scrape.py -i "$(echo "https://sozdik.kz/ru/dictionary/translate/ru/kk/"$word"/")" | html2text -utf8 -nobs
 done
